3f8f0d7fa0ea78c33c64f82bc05da080930f3465
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-06-22 13:47:27 -0300

    create fetchMovies() method inside MainActivity

--- app/src/main/java/com/example/rodri/swipedowntorefreshlistview/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/swipedowntorefreshlistview/activity/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.rodri.swipedowntorefreshlistview.activity;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.rodri.swipedowntorefreshlistview.R;
+import com.example.rodri.swipedowntorefreshlistview.app.MyApplication;
 import com.example.rodri.swipedowntorefreshlistview.helper.Movie;
 import com.example.rodri.swipedowntorefreshlistview.helper.SwipeListAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -66,5 +76,62 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         fetchMovies();
     }
 
-    
+    private void fetchMovies() {
+
+        // set refresh animation before making http call
+        swipeRefreshLayout.setRefreshing(true);
+
+        // append offSet to the URL
+        String url = URL_TOP_250_MOVIES + offSet;
+
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+
+                if (response.length() > 0) {
+
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject movieObj = response.getJSONObject(i);
+
+                            int rank = movieObj.getInt("rank");
+                            String title = movieObj.getString("title");
+
+                            Movie movie = new Movie(rank, title);
+
+                            movies.add(0, movie);
+
+                            // update offSet value
+                            if (rank >= offSet) {
+                                offSet = rank;
+                            }
+                        } catch (JSONException e) {
+                            Log.e(TAG, "JSON parsing error: " + e.getMessage());
+                        }
+                    }
+
+                    swipeListAdapter.notifyDataSetChanged();
+
+                }
+
+                // stop swipe refresh
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Server error: " + error.getMessage());
+
+                Toast.makeText(getApplicationContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                // stop swipe refresh
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        // Adding request to request queue
+        MyApplication.getInstance().addToRequestQueue(arrayRequest);
+
+    }
 }
